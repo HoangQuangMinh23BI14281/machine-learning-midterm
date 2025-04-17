8f6575c18bec3d145f4d0e3a7cde67b576a50027
HoangQuangMinh23BI14281 <minhhq.23bi14281@usth.edu.vn>
2025-04-17 23:56:09 +0700

    80% of preprocess

--- project/main.py
+++ project/main.py
@@ -9,6 +9,26 @@ data_frame = pd.read_csv('data/heart_attack_dataset.csv')
 #print(data_frame)
 
 # Preprocess the data
+data_after_ecode = preprocess.encode(data_frame)
+#print(data_after_ecode)
 data_after_preprocess, stats = preprocess.full_preprocess(data_frame)
-print(data_after_preprocess)
-print(stats)
+#print(data_after_preprocess)
+#print(stats)
+#columns_to_inverse = ['Age', 'BMI'] change the None to the columns you want to inverse standardize
+#data_after_invere_standardize = preprocess.inverse_standardize(data_after_preprocess, stats,None)
+#print(data_after_invere_standardize)
+
+X = data_after_preprocess.drop(columns=['Outcome']) # Drop the 'Outcome' column from the DataFrame
+y = data_after_preprocess['Outcome']
+
+#print(X)
+#print(y)
+
+# Apply train_test_split
+X_train, X_test, y_train, y_test = preprocess.train_test_split(X, y, test_size=0.2, seed=42)
+#print(X_train, y_train)
+#print(X_test, y_test)
+#X_train.to_csv('data/X_train.csv', index=False)
+#X_test.to_csv('data/X_test.csv', index=False)
+#y_train.to_csv('data/y_train.csv', index=False)
+#y_test.to_csv('data/y_test.csv', index=False)
